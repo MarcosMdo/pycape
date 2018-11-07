e47dbf111ca09874b4943f4c2be5870c81a14925
Marcos Mdo <Marcosmdo@knights.ucf.edu>
2018-11-07 16:52:59 -0500

    updated pycape file

--- pycape.py
+++ pycape.py
@@ -1,3 +1,4 @@
+import io
 import sys
 import numpy as np
 import math
@@ -9,6 +10,12 @@ from PIL import Image
 import os
 import errno
 import json
+from imutils.video import FPS
+import multiprocessing
+import numpy as np
+import imutils
+import dlib
+import cv2
 
 
 # PYCAPE
@@ -39,10 +46,10 @@ class Pycape:
     playersPerFrame = []
 
     def getPlayers(self):
+
         # Open image to show players
         imgFile = self.framePath + '/' + \
             str(self.currentFrame) + '.' + self.fileExt
-        print("Current directory: " + os.getcwd())
         img = mpimg.imread(imgFile)
 
         # Open detections to find bounding boxes
@@ -51,12 +58,13 @@ class Pycape:
         detFile = self.detectionPath + '/' + \
             str(self.currentFrame) + '.txt'
 
-        fp = open(str(detFile))
+        fp = open(str(os.getcwd() + '/' + detFile))
         writeFile = 'frame' + str(self.currentFrame) + 'players.txt'
+        os.chdir('playerPositions')
         f = open(writeFile, "a")
+        
         try:
             line = fp.readline()
-            cnt = 0
             while line:
 
                 line = line.split(' ', 1)[1]
@@ -71,17 +79,14 @@ class Pycape:
                 # Check for pixel dimensions. if too large, lets remove this detection.
                 # if p.x2 - p.x2 < 400 and p.y2 - p.y1 < 400:
                 players.append(p)
-                f.write(p.x1 + ' ' + p.y1 + ' ' + p.x2 + ' ' + p.y2 + '\n')
-          
-
+                f.write(p.x1 + ' ' + p.y1 + ' ' + p.x2 + ' ' + p.y2 + ' ' + p.confidence + '\n')
+                
                 line = fp.readline()
-                cnt += 1
         finally:
             fp.close()
             f.close()
             # append amount of players found in each frame.
-            self.playersPerFrame[self.currentFrame] = cnt
-
+        os.chdir(os.getcwd() + '/../')
         num = 0
         cwd = os.getcwd()
         for p in players:
@@ -105,29 +110,27 @@ class Pycape:
 
             os.chdir(bufferDirectory)
 
-            cropped_image.save(str('frame' +
-                                   str(self.currentFrame) + 'player' + str(num) + '.' + self.fileExt))
+            cropped_image.save(str('frame' + str(self.currentFrame) + 'player' + str(num) + '.' + self.fileExt))
             num += 1
             os.chdir(cwd)
             fp.close()
 
-
-    def plotPlayersToOriginal(self):
-        # Newer method to plot player positions relative to original frame image.
-        #
-        # Open the original frame, the openpose json positions, and the frame{x}players.txt file which corresponds with which detections 
-        # passed to openpose (in order).
+    def exportPlayerAnnotations(self):
+        # Method to export annotations.
+        # Open the original frame, the openpose json positions, and the frame{x}players.txt 
+        # file which corresponds with which detections passed to openpose (in order).
         
         # Image of the entire field.
         imageFile = 'YOLO/frames/' + str(self.currentFrame) + '.' + self.fileExt
 
         # x1 y1 x2 y2 positions of each player passed to openPose
-        playerPositionFile = os.getcwd() + '/playerPositions/frame' + str(self.currentFrame) + 'players.txt'
+        playerPositionFile = '/playerPositions/frame' + str(self.currentFrame) + 'players.txt'
         
-        img = mpimg.imread(imageFile)
-        fp = open(playerPositionFile, "r")
-        #plt.imshow(img)
-        plt.show()
+        # img = mpimg.imread(imageFile)
+        fp = open(os.getcwd() + playerPositionFile, "r")
+
+        outFile = 'frame' + str(self.currentFrame) + 'keypoints.json'
+        outData = []
         try:
             # read each player position line.
             line = fp.readline()
@@ -135,14 +138,100 @@ class Pycape:
             while line:
 
                 playerStats = line.split()
-                
+
                 # for each player, open its keypoints file.
                 openposeDets = 'cropOutJSON/frame' + str(self.currentFrame) + 'player' + str(cnt) + '_keypoints.json'
+                if not os.path.exists(openposeDets):
+                    line = fp.readline()
+                    continue
                 # read data in from posefile.
                 with open(openposeDets, "r") as read_file:
                     data = json.load(read_file)
+
+                frameData = [] 
                 # Parse through keypoint file to gather xy info for each limb. 
-                #print data
+                # frameData will hold the keypoints of all players detected in a frame.
+
+                if len(data['people']) != 0:
+                    it = iter(data['people'][0]['pose_keypoints_2d'])
+                    for x in it:
+                        try:
+
+                            # Repurpose its position with comparison to where on the main image it is.
+                            # include 25 pixel buffer added.
+                            #print 'player stats: x value: ' + playerStats[0]
+
+                            x1 = int(x)
+                            y1 = int(next(it))
+                            if x1 == 0 and y1 == 0:
+                                next(it)
+                                continue
+
+                            x1 = x1 - 25
+                            x1 = x1 + int(playerStats[0])
+
+                            y1 = y1 - 25
+                            y1 = y1 + int(playerStats[1])
+
+                            frameData.append((x1, y1))
+                            #print frameData
+                            #plt.scatter([x1], [y1])  # x1 and y1
+                            #plt.imshow(img)
+                            
+                            next(it)  # skip over confidence level       
+                        except:
+                            StopIteration
+                            print 'error', sys.exc_info()[0]
+                else:
+                    print 'No openpose detection found on' +  openposeDets
+                
+                line = fp.readline()
+                cnt += 1
+                outData.append(frameData)
+        finally:
+            fp.close()
+            print 'closed player position file.'
+        os.chdir(str(os.getcwd() + '/trackingKeypoints'))
+        with open(outFile, 'w') as outfile:
+            json.dump(outData, outfile, indent=4, separators=(',', ': '))
+        os.chdir(str(os.getcwd()) + '/../')
+        # plt.imshow(img)
+        # plt.show()
+
+    def plotPlayersToOriginal(self):
+        # Method to plot player positions relative to original frame image.
+        #
+        # Open the original frame, the OpenPose JSON positions, and the frame{x}players.txt 
+        # file which corresponds with the YOLO bounding boxes.
+        
+        # Image of the entire field.
+        imageFile = 'YOLO/frames/' + str(self.currentFrame) + '.' + self.fileExt
+        img = mpimg.imread(imageFile)
+
+        # x1 y1 x2 y2 positions of each player passed to openPose from YOLO.
+        playerPositionFile = '/playerPositions/frame' + str(self.currentFrame) + 'players.txt'
+        fp = open(os.getcwd() + playerPositionFile, "r")
+
+        # plt.imshow(img) # plots the original image. 
+        # plt.show() # Shows plotted area.
+
+        try:
+            # read each player position line from YOLO bounding boxes. 
+            line = fp.readline()
+            cnt = 0
+            while line:
+
+                playerStats = line.split()
+                
+                # for each player, open its OpenPose keypoints file.
+                openposeDets = 'cropOutJSON/frame' + str(self.currentFrame) + 'player' + str(cnt) + '_keypoints.json'
+                
+                # read data in from posefile.
+                with open(openposeDets, "r") as read_file:
+                    data = json.load(read_file)
+
+                # Parse through keypoint file to gather xy info for each limb. 
+
                 if len(data['people']) != 0:
                     it = iter(data['people'][0]['pose_keypoints_2d'])
                     for x in it:
@@ -156,24 +245,21 @@ class Pycape:
                             if x1 == 0 and y1 == 0:
                                 next(it)
                                 continue
-                            #print 'x before adjustment: ' + x1
+
                             x1 = x1 - 25
                             x1 = x1 + int(playerStats[0])
-
                             
-                            #print 'y before adjustment: ' + y1
                             y1 = y1 - 25
                             y1 = y1 + int(playerStats[1])
 
-                            #print 'x1: ' + str(x1) + ' y1: ' + str(y1)
+                            #print 'x1: ' + str(x1) + ' y1: ' + str(y1) 
                             plt.scatter([x1], [y1])  # x1 and y1
                             #plt.imshow(img)
                             
-                            next(it)  # skip over confidence level       
+                            conf = next(it)  # skip over limb confidence level. saving just in case needed in future.
                         except:
                             StopIteration
                             print 'error', sys.exc_info()[0]
-                
                 else:
                     print 'No openpose detection found on' +  openposeDets
                 
@@ -182,8 +268,15 @@ class Pycape:
         finally:
             fp.close()
             print 'closed player position file.'
-        plt.imshow(img)
-        plt.show()
+        
+        # plt.imshow(img)
+        # plt.show()
+
+        os.chdir('OutputFrames')
+        filename = str(self.currentFrame) + '.jpg'
+        plt.savefig(filename, format='jpg', dpi=1200)
+        os.chdir('../')
+        plt.close()
 
     def plotPlayers(self):
         # Currently this method is written to open the individually cropped images of openpose and plot the annotated points.
@@ -230,28 +323,35 @@ class Pycape:
                 
                 #plt.show()
 
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-f', '--framePath', required=True)
     parser.add_argument('-d', '--detectionPath', required=True)
     parser.add_argument('-n', '--numFrames', required=True)
     parser.add_argument('-e', '--ext', required=True)
+    parser.add_argument('-m', '--mode', required=True)
 
     parsed = parser.parse_args()
     pycape = Pycape()
 
     pycape.framePath = str(parsed.framePath)
     pycape.detectionPath = str(parsed.detectionPath)
-    pycape.numFrames = parsed.numFrames
+    pycape.numFrames = int(parsed.numFrames)
     pycape.fileExt = str(parsed.ext)
-    pycape.currentFrame = 2
-    pycape.playersPerFrame = [0,0,0,0,0,0,0,0,0,0,0]
+    pycape.currentFrame = 1
 
-    # for i in range(1, 11):
-    #     pycape.currentFrame = i
-    #     pycape.getPlayers()
+    if str(parsed.mode) == 'crop':
+        for i in range(1,pycape.numFrames + 1):
+            pycape.currentFrame = i
+            pycape.getPlayers()
+    elif str(parsed.mode) == 'export':
+        for i in range(1, pycape.numFrames + 1):
+            pycape.currentFrame = i
+            #pycape.plotPlayersToOriginal()
+            pycape.exportPlayerAnnotations()
 
-    pycape.plotPlayersToOriginal()
+    #pycape.plotPlayersToOriginal()
 
 
 # Will only run if this file is called as primary file
